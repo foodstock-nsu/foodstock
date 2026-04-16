fc79aa8b271b1270ace1937312c2040ba2bef4ca
Зябкин Владимир <vovvovin5124@gmail.com>
2026-04-16 17:35:43 +0700

    define sql operations for order repository

--- backend/internal/domain/port/location_item_repository_interface.go
+++ backend/internal/domain/port/location_item_repository_interface.go
@@ -8,10 +8,10 @@ import (
 )
 
 type LocationItemRepository interface {
-	Create(ctx context.Context, locItem *model.LocationItem) error
+	Create(ctx context.Context, locationItem *model.LocationItem) error
 	GetByID(ctx context.Context, id uuid.UUID) (*model.LocationItem, error)
 	GetByLocationAndItem(ctx context.Context, locationID, itemID uuid.UUID) (*model.LocationItem, error)
-	Update(ctx context.Context, locItem *model.LocationItem) error
+	Update(ctx context.Context, locationItem *model.LocationItem) error
 	Delete(ctx context.Context, id uuid.UUID) error
 	List(ctx context.Context, locationID uuid.UUID, limit, offset int) ([]*model.LocationItem, error)
 }
